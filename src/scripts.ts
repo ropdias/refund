ac35744463576cbd1b70b7f4f7e4085a91aa5225
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-06-07 23:51:41 -0300

    formatting currency

--- src/scripts.ts
+++ src/scripts.ts
@@ -3,10 +3,21 @@ const amount = document.getElementById('amount') as HTMLInputElement;
 
 if (amount) {
   amount.oninput = () => {
+    // Get input value and remove non-numeric characters
     let value = amount.value.replace(/\D/g, '');
 
-    amount.value = value;
+    // Transform the value with cents (example: 150/100 = 1.5 equivalent to R$ 1,50)
+    let valueWithCents = Number(value) / 100;
+
+    amount.value = formatCurrencyBRL(valueWithCents);
   };
 } else {
   console.error('Um ou mais elementos não foram encontrados.');
+}
+
+function formatCurrencyBRL(value: number): string {
+  return value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
 }
